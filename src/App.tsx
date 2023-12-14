7f54b2b524eeb1b7d465c2d2ae5f62783c923e01
nomad <nomad>
2023-12-14 11:24:08 -0600

    open user profiles in new tab

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const Welcome = () => {
 const Post = ({id, text, author, when}: {id: number, text: string, author: string, when: string}) => {
   return (
     <>
-      <span style={{"float": "right"}}><i><a href={`https://news.ycombinator.com/user?id=${author}`}>{author}</a> on {when.split('T')[0]}</i></span>
+      <span style={{"float": "right"}}><i><a target='_blank' href={`https://news.ycombinator.com/user?id=${author}`}>{author}</a> on {when.split('T')[0]}</i></span>
       <p key={id} dangerouslySetInnerHTML={{"__html": text.replaceAll('\n', '<br />')}} />
       <hr />
     </>
